8427960f7ac7fa6ef7e7e735ff3e83213cc82daf
Garima jain <garimajain1704@gmail.com>
2026-01-31 15:39:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Project
+      </h1>
+
+      <h1 className="text-xl font-medium text-gray-700">
+        wdweugdwg
+      </h1>
+    </div>
+  );
+}
+
+export default App;
